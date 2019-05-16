07f74bb4721ae59b0c554bf2e3353df0ddc2079c
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-05-16 17:39:22 +0800

    Initial Commit

---
+++ TimeCountDownDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeCountDownDemo
+//
+//  Created by APP技術部-陳冠志 on 2019/5/16.
+//  Copyright © 2019 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
